79973cc5cb19375b2a4463b8c6968233ba35f78a
JanEricStorms <jan-eric.storms@outlook.de>
2021-10-13 23:21:11 +0200

    abc

--- Git-Blockkurs/src/test.java
+++ Git-Blockkurs/src/test.java
@@ -1,14 +1,16 @@
 
 public class test {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char c = 'c';
-		String s = String.valueOf(c);
-		System.out.println(s);
-		s = Integer.toString(1);
-		System.out.println(s);
-		System.out.println("+---+---+---+---+---+---+---+---+\n"+"| b |\n+---+---+---+---+---+---+---+---+  ");
+		double r = Math.sqrt(Math.pow(-4, 2)+Math.pow(0, 2));
+		double phi = Math.atan(2/-4) + Math.PI;
+		String[] arr = new String[2];
+		arr[0] = "" +Math.sqrt(r)*(Math.cos((phi+2*0*Math.PI)/2))+ " " +Math.sqrt(r)*(Math.sin((phi+2*0*Math.PI)/2));
+		arr[1] =  "" +Math.sqrt(r)*(Math.cos((phi+(2*Math.PI))/2))+ " " +Math.sqrt(r)*(Math.sin((phi+(2*Math.PI))/2));
+		System.out.println(arr[0].toString());
+		System.out.println(arr[1].toString());
 	}
 
 }
